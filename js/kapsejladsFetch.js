d11dbfa41aa56cc6860c8fb48369c657b2f6d73a
CodeClod <mika9103@stud.kea.dk>
2023-06-05 22:39:50 +0200

    Fjernet sejlads-deltager Id fra Table og andre tweaks

--- js/kapsejladsFetch.js
+++ js/kapsejladsFetch.js
@@ -3,6 +3,7 @@
 
 const urlGetKapsejlads = 'http://localhost:8080/kapsejladsInOrder'; // Replace with the appropriate endpoint for fetching Kapsejlads entities
 const kapsejladsSelect = document.getElementById('kapsejladsSelect');
+const kapsejladsTableSelect = document.getElementById("kapsejladsTableSelect");
 
 async function fetchKapsejlads() {
     try {
@@ -14,7 +15,7 @@ async function fetchKapsejlads() {
         data.forEach((kapsejlads) => {
             const option = document.createElement('option');
             option.value = kapsejlads.kapsejladsId;
-            option.textContent = kapsejlads.navn; // Replace with the appropriate property for displaying the Kapsejlads name
+            option.textContent = kapsejlads.navn;
             kapsejladsSelect.appendChild(option);
         });
     } catch (error) {
@@ -22,4 +23,23 @@ async function fetchKapsejlads() {
     }
 }
 
+async function fetchKapsejladsTable() {
+    try {
+        const response = await fetch(urlGetKapsejlads);
+        if (!response.ok) {
+            throw new Error('Error fetching KapsejladsTableSelect entities');
+        }
+        const data = await response.json();
+        data.forEach((kapsejlads) => {
+            const htmlOptionElement = document.createElement('option');
+            htmlOptionElement.value = kapsejlads.kapsejladsId;
+            htmlOptionElement.textContent = kapsejlads.navn;
+            kapsejladsTableSelect.appendChild(htmlOptionElement);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 fetchKapsejlads();
+fetchKapsejladsTable();
--- js/sejladsdeltager.js
+++ js/sejladsdeltager.js
@@ -1,9 +1,10 @@
 console.log("KapsejladsDeltagerForm");
 
 const urlCreateKapsejladsDeltager = 'http://localhost:8080/kapsejladsdeltager/create';
-const urlGetKapsejladsdeltagereByKapsejlads = 'http://localhost:8080/kapsejladsdeltager/getAllByKapsejlads';
+
 
 document.addEventListener('DOMContentLoaded', createFormEventListener);
+document.addEventListener('DOMContentLoaded', loadKapsejladsDeltagers);
 let kapsejladsDeltagerCreateForm;
 
 function createFormEventListener(){
@@ -54,130 +55,29 @@ async function postFormData(url, formData) {
     }
     return response.json();
 }
+function loadKapsejladsDeltagers() {
+    var kapsejladsId = document.getElementById("kapsejladsTableSelect").value;
 
-function convertFormDataToJson(formData) {
-    const plainFormData = Object.fromEntries(formData.entries());
-    const formDataJsonString = JSON.stringify(plainFormData);
-    console.log(formDataJsonString);
-    return formDataJsonString;
+    fetch("http://localhost:8080/kapsejladsdeltager/getAllByKapsejladsId/" + kapsejladsId)
+        .then(response => response.json())
+        .then(data => {
+            var table = document.getElementById("kapsejladsDeltagerTable");
+            while (table.rows.length > 1) {
+                table.deleteRow(1);
+            }
+
+            // Populate the table with the fetched KapsejladsDeltagers
+            data.forEach(kapsejladsDeltager => {
+                var row = table.insertRow();
+                row.insertCell().textContent = kapsejladsDeltager.sejlbåd.navn;
+                row.insertCell().textContent = kapsejladsDeltager.points;
+                row.insertCell().textContent = kapsejladsDeltager.placering;
+                // Add more cells as needed for additional columns
+            });
+        })
+        .catch(error => console.error('Error:', error));
 }
 
-async function postFormDataAsJson(url, jsonToSend) {
-    const fetchOptions = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: jsonToSend,
-    };
-    console.log("Calling URL: " + url);
-    const response = await fetch(url, fetchOptions);
-    if (!response.ok) {
-        const errorMessage = await response.text();
-        throw new Error(errorMessage);
-    }
-    return response.json();
-}
+const kapsejlsDeltagerList = document.getElementById("kapsejladsTableSelect");
 
-async function updateDeltager(updatedDeltager) {
-    const urlUpdateDeltager = `http://localhost:8080/deltagere/${updatedDeltager.kapsejladsDeltagerId}`;
-    const fetchOptions = {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(updatedDeltager)
-    };
-
-    try {
-        const response = await fetch(urlUpdateDeltager, fetchOptions);
-        if (!response.ok) {
-            const errorMessage = await response.text();
-            throw new Error(errorMessage);
-        }
-
-        console.log('Deltager updated successfully');
-    } catch (error) {
-        alert('Error updating deltager: ' + error.message);
-        console.error(error);
-    }
-}
-
-async function deleteDeltager(deltagerId) {
-    const urlDeleteDeltager = `http://localhost:8080/deltagere/${deltagerId}`;
-
-    const fetchOptions = {
-        method: 'DELETE'
-    };
-
-    try {
-        const response = await fetch(urlDeleteDeltager, fetchOptions);
-        if (!response.ok) {
-            const errorMessage = await response.text();
-            throw new Error(errorMessage);
-        }
-
-        console.log('Deltager deleted successfully');
-    } catch (error) {
-        alert('Error deleting deltager: ' + error.message);
-        console.error(error);
-    }
-}
-
-/*
-const deltagerTable = document.getElementById('deltagerTable')
-
-async function createDeltagerTable(deltager) {
-    const row = document.createElement('tr');
-    const idCell = document.createElement('td');
-    idCell.textContent = deltager.kapsejladsDeltagerId;
-    idCell.value = deltager.kapsejladsDeltagerId;
-    row.appendChild(idCell);
-
-    const pointsInput = document.createElement('input');
-    pointsInput.type = 'text';
-    pointsInput.value = deltager.points;
-    row.insertCell().appendChild(pointsInput);
-
-    const updateButton = document.createElement('button');
-    updateButton.textContent = 'Opdater';
-    updateButton.className = 'buttonupdate';
-    updateButton.addEventListener('click', async function() {
-        const updatedDeltager = {
-            kapsejladsDeltagerId: idCell.value,
-            points: pointsInput.value
-        };
-        await updateDeltager(updatedDeltager);
-        fetchDeltagere();
-    });
-    row.insertCell().appendChild(updateButton);
-
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Slet';
-    deleteButton.className = 'buttondelete';
-    deleteButton.addEventListener('click', async function() {
-        const confirmation = confirm('Er du sikker på, at du vil slette denne deltager?');
-        if (confirmation) {
-            await deleteDeltager(deltager.kapsejladsDeltagerId);
-            fetchDeltagere();
-        }
-    });
-    row.insertCell().appendChild(deleteButton);
-
-    deltagerTable.appendChild(row);
-}
-
-let lstDeltagere = [];
-async function fetchDeltagere() {
-    lstDeltagere = await fetchAny(urlGetKapsejladsdeltagereByKapsejlads);
-    deltagerTable.innerHTML = '';
-    lstDeltagere.forEach(createDeltagerTable);
-}
-
-function printDeltager(deltagerId, deltager) {
-    console.log(deltagerId);
-    console.log(deltager);
-}
-
-fetchDeltagere();
-*/
+kapsejlsDeltagerList.addEventListener(onchange, loadKapsejladsDeltagers);
